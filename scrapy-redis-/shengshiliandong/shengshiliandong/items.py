8d335c706daef3aac0e0ccbb4d736a94cf9a728c
wangtianshang <1241814506@qq.com>
2018-11-05 18:26:14 +0800

    1

---
+++ scrapy-redis-/shengshiliandong/shengshiliandong/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ShengshiliandongItem(scrapy.Item):
+    # define the fields for your item here like:
+    first_city = scrapy.Field()
+    second_city = scrapy.Field()
+    third_city = scrapy.Field()
+    fouth_city = scrapy.Field()
+    fifth_city = scrapy.Field()
+    pass
